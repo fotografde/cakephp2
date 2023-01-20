c87da26947cd352bc0e9eee62cdd46ccce417fb9
Marco Beinbrech <marco.beinbrech@fotograf.de>
2023-01-20 17:32:10 +0100

    added docker compose config

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -109,3 +109,5 @@ CakeLog::config('error', array(
 	'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
 	'file' => 'error',
 ));
+
+require_once dirname(__DIR__, 2) . DS . 'vendor/autoload.php';
